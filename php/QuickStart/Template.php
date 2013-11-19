c73e3f5738cfb2f2138b3506dae899f18a2b37e2
dougwollison <dougwollison@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-19 04:27:15 +0000

    Version 1.1.0 release, pulled from GitHub
    git-svn-id: https://plugins.svn.wordpress.org/quickstart/trunk@806556 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/QuickStart/Template.php
+++ php/QuickStart/Template.php
@@ -16,21 +16,22 @@ class Template {
 	 * Doctype and opening html tag with
 	 * IE conditional comments for classes.
 	 *
+	 * @sicne 1.1.0 Fixed IE9 tagging.
 	 * @since 1.0.0
 	 */
 	public static function doc_start() {
 		?><!DOCTYPE html>
 <!--[if lte IE 6]>
-<html id="ie6" class="ie8- ie7- ie6-" <?php language_attributes(); ?>>
+<html id="ie6" class="ie9- ie8- ie7- ie6-" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 7]>
-<html id="ie7" class="ie8- ie7-" <?php language_attributes(); ?>>
+<html id="ie7" class="ie9- ie8- ie7-" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 8]>
-<html id="ie8" class="ie8-" <?php language_attributes(); ?>>
+<html id="ie8" class="ie9- ie8-" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 9]>
-<html id="ie8" class="ie8-" <?php language_attributes(); ?>>
+<html id="ie9" class="ie9-" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 6) | !(IE 7) | !(IE 8) | !(IE 9)  ]><!-->
 <html <?php language_attributes(); ?>>
