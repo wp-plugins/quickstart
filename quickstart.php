4cb5ed920ad1f98b3d14724f506aeec68fe6215a
dougwollison <dougwollison@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 14:42:03 +0000

    File restructuring and patch for 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/quickstart/trunk@993302 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- quickstart.php
+++ quickstart.php
@@ -3,7 +3,7 @@
 Plugin Name: QuickStart
 Plugin URI: https://github.com/dougwollison/quickstart
 Description: A utility kit for quick development of WordPress themes (and plugins). ***YOUR CUSTOM THEME RELIES ON THIS PLUGIN***
-Version: 1.6.0
+Version: 1.6.1
 Author: Doug Wollison
 Author URI: http://dougw.me
 License: GPL2
@@ -22,18 +22,29 @@ $QuickStart = null;
 define( 'QS_FILE', __FILE__ );
 define( 'QS_DIR', __DIR__ );
 
-// Load functions
+// Require includes
 require ( QS_DIR . '/php/aliases.php' );
 require ( QS_DIR . '/php/constants.php' );
 require ( QS_DIR . '/php/utilities.php' );
 
-// Load classes
-require ( QS_DIR . '/php/SmartPlugin.php' );
-require ( QS_DIR . '/php/QuickStart/Callbacks.php' );
-require ( QS_DIR . '/php/QuickStart/Exception.php' );
-require ( QS_DIR . '/php/QuickStart/Features.php' );
-require ( QS_DIR . '/php/QuickStart/Form.php' );
-require ( QS_DIR . '/php/QuickStart/Hooks.php' );
-require ( QS_DIR . '/php/QuickStart/Setup.php' );
-require ( QS_DIR . '/php/QuickStart/Template.php' );
-require ( QS_DIR . '/php/QuickStart/Tools.php' );
+// Class autoloader
+spl_autoload_register( function( $class ) {
+	// Split it to get the Namespace and Class names, in reverse order
+	$class_parts = array_reverse( explode( '\\', $class ) );
+
+	// Get just the class
+	$class = $class_parts[0];
+
+	// Reformat to wordpress standards
+	$file = 'class-' . strtolower( str_replace( '_', '-', $class ) ) . '.php';
+
+	// If a namespace exists, prepend it
+	if ( isset( $class_parts[1] ) ) {
+		$file = strtolower( str_replace( '_', '-', $class_parts[1] ) ) . '/' . $file;
+	}
+
+	// Make sure the file exists before loading it
+	if ( file_exists ( plugin_dir_path( __FILE__ ) . 'php/' . $file ) ){
+        include( plugin_dir_path( __FILE__ ) . 'php/' . $file );
+    }
+});
